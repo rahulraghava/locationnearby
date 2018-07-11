4af5f4342a62edfda396c8c7cb3b06b419230f60
punith <x17114331@student.ncirl.ie>
2018-07-11 01:35:40 +0100

    4 upload

--- flas.py
+++ flas.py
@@ -1,11 +1,15 @@
 from flask import Flask 
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World"
+    con = sqlite3.connect('database.db')
+    c = con.cursor()
+    c.execute("SELECT * FROM stocks")
+    return c.fetchone()[0]
     
 if __name__ == "__main__":
     app.run()
