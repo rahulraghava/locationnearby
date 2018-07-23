9fd9bb1481fc818fac7f53516f6730e3c79398ff
punith <x17114331@student.ncirl.ie>
2018-07-23 23:21:28 +0100

    final deployment

--- flas.py
+++ flas.py
@@ -1,5 +1,9 @@
 from flask import Flask 
+from flask import request
 import sqlite3
+import requests
+import json
+import req
 
 app = Flask(__name__)
 
@@ -8,8 +12,19 @@ app = Flask(__name__)
 def index():
     con = sqlite3.connect('database.db')
     c = con.cursor()
+    x = request.args.get('citi', default = '*', type = str)
+    if x == "*":
+        return "error citis missing"
     c.execute("SELECT * FROM stocks")
-    return c.fetchone()[0]
+    url = 'https://geocoder.cit.api.here.com/6.2/geocode.json?app_id=LGKseBzhB368K4hkIZ7c&app_code=jj1R0tMgUUquGiZ6oe_h3Q&searchtext=' + x
+    r = requests.get(url)
+    a = r.json()
+    if len(a['Response']['View']) == 0:
+        return json.dumps({'response':[]})
+    c = a['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Longitude']
+    b = a['Response']['View'][0]['Result'][0]['Location']['DisplayPosition']['Latitude']
+    d = "Longitude" + str(c) + " & Latitude" + str(b)
+    return req.output(b,c)
     
 if __name__ == "__main__":
     app.run()
---
+++ req.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def output(lt,lg):
+
+    loc = ["amusement_park","aquarium","art_gallery","library","movie_theater","museum","zoo"]
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=1500&keyword=cruise&key=AIzaSyB5xoYAKvc0G0A7Q3VV7KEghyVUQdfJuD0&location=' + str(lt) + ',' + str(lg) + '&type='
+    url2 = 'https://reverse.geocoder.cit.api.here.com/6.2/reversegeocode.json?app_id=LGKseBzhB368K4hkIZ7c&app_code=jj1R0tMgUUquGiZ6oe_h3Q&mode=retrieveLandmarks&prox=51.50643,-0.12721,1000'
+
+
+    final = []  
+    for a in range(len(loc)):
+        e = requests.get(url + loc[a])
+        res = e.json()
+
+        if res['status'] != 'OK':
+            continue
+        for i in range(len(res['results'])):
+            url2 = 'https://reverse.geocoder.cit.api.here.com/6.2/reversegeocode.json?app_id=LGKseBzhB368K4hkIZ7c&app_code=jj1R0tMgUUquGiZ6oe_h3Q&mode=retrieveLandmarks&prox=' + str(res['results'][i]['geometry']['location']['lat']) + ',' + str(res['results'][i]['geometry']['location']['lng']) + ',1000'
+            s = requests.get(url2)
+            w = s.json()
+            p = {'name': res['results'][i]['name'],'lat':res['results'][i]['geometry']['location']['lat'],'lng':res['results'][i]['geometry']['location']['lng'],'address':w['Response']['View'][0]['Result'][0]['Location']['Address']['Label'],'type':loc[a]}
+            final.append(p)
+    rep = {'response':final}
+    z = json.dumps(rep)
+    print(z)
+    return z
+
